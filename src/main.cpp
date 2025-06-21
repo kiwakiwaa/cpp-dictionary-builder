5e1f0c1500b545f5097428b5ffb399caf72063da
kiwakiwaa <caoimhe.hp@proton.me>
2025-06-21 12:54:19 +0200

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -5,29 +5,6 @@
 
 #include <iostream>
 
-/**
-int main()
-{
-    ParserRegistry::registerAllParsers();
-
-    const auto configLoader = ConfigLoader::loadFromFile("resources/dictionaries.yaml");
-    auto [yomitanConfig, parserConfig] = configLoader.getDictionaryInfo("YDP");
-    auto dictionary = std::make_unique<YomitanDictionary>(yomitanConfig);
-
-    auto& registry = ParserRegistry::getInstance();
-    const auto parser = registry.createParser("YDP", std::move(dictionary), parserConfig);
-
-    const int parsedEntries = parser->parse();
-    std::cout << "Parsed entries: " << parsedEntries << std::endl;
-
-    if ([[maybe_unused]] auto result = parser->exportDictionary("/Users/caoimhe/Downloads/test-dictionary"))
-    {
-        std::cout << "Successfully exported dict to specified path" << std::endl;
-    }
-    return 0;
-}
-*/
-
 int main()
 {
     const auto configLoader = ConfigLoader::loadFromFile("resources/dictionaries.yaml");
